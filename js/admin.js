7eae0293db4cdf9267492ead6f5508320142cc48
Mickemannen86 <112933571+Mickemannen86@users.noreply.github.com>
2023-02-08 16:16:00 +0100

    gewge

--- js/admin.js
+++ js/admin.js
@@ -89,8 +89,8 @@ function getAdminContent(content) {
                     "arrayValue": {
                         "values": [{
                             "stringValue": id
-                            }//,
-                        ],
+                            }
+                        ]
                     } 
                 }
             }
